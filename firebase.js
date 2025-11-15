5c0113549100a2b81e2566c2752218c32f2c0322
Djellza Jasiqi <150348910+DiellzaJ@users.noreply.github.com>
2025-11-15 20:32:23 +0100

    adding the firebase file and the auth on the signin

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtbV_kbZsxHKDWWQjijXQhplnZLa6mBoY",
+  authDomain: "piqe-hiqe.firebaseapp.com",
+  projectId: "piqe-hiqe",
+  storageBucket: "piqe-hiqe.firebasestorage.app",
+  messagingSenderId: "933922632992",
+  appId: "1:933922632992:web:3de5d5731edbb8e3f79345"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { auth };
